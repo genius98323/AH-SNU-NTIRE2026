9cd3c8150fe2535d6054e556f35ffb36402dd49d
genius98323 <amiteshmahendran@gmail.com>
2026-03-17 22:11:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -12,19 +12,35 @@ from utils import utils_image as util
 
 
 def select_model(args, device):
-    # Model ID is assigned according to the order of the submissions.
-    # Different networks are trained with input range of either [0,1] or [0,255]. The range is determined manually.
     model_id = args.model_id
-    if model_id == 0:
-        # DAT baseline, ICCV 2023
-        from models.team00_DAT import main as DAT
-        name = f"{model_id:02}_DAT_baseline"
-        model_path = os.path.join('model_zoo', 'team00_dat.pth')
-        model_func = DAT
-    else:
-        raise NotImplementedError(f"Model {model_id} is not implemented.")
 
-    return model_func, model_path, name
+    if model_id == 33:
+        from models.33_HAT_FFL.hat_ffl_model import Model
+
+        name = "33_HAT_FFL"
+        model_path = os.path.join('model_zoo', '33_HAT_FFL', 'net_g_50000.pth')
+
+        def model_func(model_dir, input_path, output_path, device):
+            import glob, cv2
+            model = Model(model_dir, device)
+
+            paths = sorted(glob.glob(os.path.join(input_path, '*')))
+
+            for p in paths:
+                img = cv2.imread(p)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+                out = model.process(img)
+
+                out = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
+
+                name = os.path.basename(p)
+                cv2.imwrite(os.path.join(output_path, name), out)
+
+        return model_func, model_path, name
+
+    else:
+        raise NotImplementedError(f"Model {model_id} not implemented")
 
 
 def run(model_func, model_name, model_path, device, args, mode="test"):
@@ -87,7 +103,7 @@ if __name__ == "__main__":
     parser.add_argument("--valid_dir", default=None, type=str, help="Path to the validation set")
     parser.add_argument("--test_dir", default=None, type=str, help="Path to the test set")
     parser.add_argument("--save_dir", default="NTIRE2025-ImageSRx4/results", type=str)
-    parser.add_argument("--model_id", default=0, type=int)
+    parser.add_argument("--model_id", default=33, type=int)
 
     args = parser.parse_args()
     pprint(args)
